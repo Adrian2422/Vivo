74a70086f8f90e815758c49cc03a05bf7ccbd90d
Adrian Kloc <adrian.kloc47@gmail.com>
2026-08-26 21:26:55 +0200

    domain: Value Objects introduction

---
+++ src/Vivo.Domain/ValueObjects/TargetUrl.cs
@@ -0,0 +1,22 @@
+﻿namespace Vivo.Domain.ValueObjects;
+
+public record TargetUrl
+{
+    public string Value { get; }
+
+    public TargetUrl(string value)
+    {
+        if (string.IsNullOrWhiteSpace(value))
+            throw new ArgumentException("URL cannot be empty.", nameof(value));
+
+        if (!Uri.TryCreate(value, UriKind.Absolute, out var uri))
+            throw new ArgumentException("URL is not well-formed.", nameof(value));
+
+        if (uri.Scheme != Uri.UriSchemeHttp && uri.Scheme != Uri.UriSchemeHttps)
+            throw new ArgumentException("Only http and https schemes are allowed.", nameof(value));
+
+        Value = value;
+    }
+
+    public override string ToString() => Value;
+}
